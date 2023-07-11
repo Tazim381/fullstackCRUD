14096ad7250fa7d8ef665ee953282856c3a7e509
Tazim381 <jucse28.381@gmail.com>
2023-07-12 00:49:08 +0600

    frontEnd ,add task,delete task added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect } from 'react'
+import {useState} from 'react'
+import axios from 'axios'
+const  App = () => {
+  const[input,setInput] = useState("")
+  const[tasks,setTask] = useState([])
+  const[update,setUpdate] = useState(false)
 
-function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    fetch("http://localhost:5000/getTask")
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      setTask(data)
+    }
+      )
+  },[input,update])
+
+  const addTask =() =>{
+    axios.post('http://localhost:5000/setTask', {
+      task: input,
+    })
+    .then( (response)=> {
+      console.log(response);
+      setInput('')
+    })
+    .catch((error) =>{
+      console.log(error);
+    });
+  }
+
+  const deleteTask =(id)=>{
+    axios.delete(`http://localhost:5000/deleteTask/${id}`)
+    .then((res) => {
+      console.log(res)
+      setUpdate((previous) => !previous)
+    })
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <div className='flex flex-col justify-center items-center'>
+    <h1 className='text-blue-500'>Full Stack Crud</h1>
+    <div className='flex gap-5'>
+    <input className='border-2 border-sky-500'type="text" onChange={(e) => {setInput(e.target.value) }}/>
+    <button type="submit" className='border-2 border-sky-500 p-1' onClick={addTask}>Add task</button>
+    </div>
+    <div>
+    {
+    tasks.map((task,index) =>(
+      <li>{task.task}
+      <button className="border-2 border-red-500 ml-8 mt-5" onClick={()=>deleteTask(task._id)}>Delete</button>
+      
+      </li>
+      
+    ))
+    }
+    </div>
+   </div>
+
+
   )
 }
 
