9e10047d13916fa7f159b0802d235ec1226fd6b0
Tazim381 <jucse28.381@gmail.com>
2023-07-14 16:52:00 +0600

    full crud added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,9 @@ import axios from 'axios'
 const  App = () => {
   const[input,setInput] = useState("")
   const[tasks,setTask] = useState([])
-  const[update,setUpdate] = useState(false)
+  const[updateUI,setUpdateUI] = useState(false)
+  const[updateMode,setUpdateMode] = useState(false)
+  const [updateId, setUpdateId] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:5000/getTask")
@@ -14,13 +16,14 @@ const  App = () => {
       setTask(data)
     }
       )
-  },[input,update])
+  },[input,updateUI])
+
 
   const addTask =() =>{
     axios.post('http://localhost:5000/setTask', {
       task: input,
     })
-    .then( (response)=> {
+    .then((response)=> {
       console.log(response);
       setInput('')
     })
@@ -33,25 +36,42 @@ const  App = () => {
     axios.delete(`http://localhost:5000/deleteTask/${id}`)
     .then((res) => {
       console.log(res)
-      setUpdate((previous) => !previous)
+      setUpdateUI((updateUI) => !updateUI)
     })
+  }
+
+  const updateTask =()=>{
+    axios.put(`http://localhost:5000/updateTask/${updateId}`, {
+      task:input,
+    }).then((res) => {
+      console.log(res)
+      setUpdateUI((updateUI) => !updateUI)
+      setUpdateId(null);
+      setUpdateMode(false)
+      setInput("")
+    })
+  }
+  
+  const update =(id,task) =>{
+    setInput(task)
+    setUpdateMode(true)
+    setUpdateId(id)
   }
 
   return (
    <div className='flex flex-col justify-center items-center'>
     <h1 className='text-blue-500'>Full Stack Crud</h1>
     <div className='flex gap-5'>
-    <input className='border-2 border-sky-500'type="text" onChange={(e) => {setInput(e.target.value) }}/>
-    <button type="submit" className='border-2 border-sky-500 p-1' onClick={addTask}>Add task</button>
+    <input className='border-2 border-sky-500'type="text" value={input} onChange={(e) => {setInput(e.target.value) }}/>
+    <button type="submit" className='border-2 border-sky-500 p-1' onClick={updateMode ? updateTask : addTask}>{updateMode ? "Update" : "ADD"}</button>
     </div>
     <div>
     {
     tasks.map((task,index) =>(
       <li>{task.task}
+      <button className="border-2 border-green-500 ml-8 mt-5" onClick={()=>update(task._id,task.task)}>Update</button>
       <button className="border-2 border-red-500 ml-8 mt-5" onClick={()=>deleteTask(task._id)}>Delete</button>
-      
-      </li>
-      
+      </li> 
     ))
     }
     </div>
